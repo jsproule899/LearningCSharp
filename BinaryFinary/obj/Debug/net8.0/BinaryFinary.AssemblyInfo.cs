73cee3048337783172f519aede98e6acd2ded26e
Joshua Sproule <joshsproule@live.co.uk>
2024-05-28 23:38:18 +0100

    ASP.net MVC  Movie Demo

--- BinaryFinary/obj/Debug/net8.0/BinaryFinary.AssemblyInfo.cs
+++ BinaryFinary/obj/Debug/net8.0/BinaryFinary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BinaryFinary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ff8f86e5cf927972ba50e4b38384fadecae4a0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c23b3aecfb5f6b62f5ff9e1eb58ffe56a65f0be")]
 [assembly: System.Reflection.AssemblyProductAttribute("BinaryFinary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BinaryFinary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
